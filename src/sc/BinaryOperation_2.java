ac02e76af20e067fbd65f66065dd876c0dfabbaa
201720307 <tian.jingqi@qq.com>
2019-11-14 11:22:49 +0800

    实验一第二题

--- src/sc/BinaryOperation_2.java
+++ src/sc/BinaryOperation_2.java
@@ -4,11 +4,13 @@ import java.util.Random;
 
 public class BinaryOperation_2 {
 	public static void main(String[] args) {
+		//主函数
 		short m=0,n=0,ov=0,q=0;
 		char o='+';
 		calculate(m,n,ov,q,o);
 	}
 	public static void calculate(short m,short n,short ov,short q,char o){
+		//输出式子的函数
 		
 		Random random=new Random();
 		for(int i=0;i<200;i++){
@@ -31,12 +33,14 @@ public class BinaryOperation_2 {
 			if(q>50)
 				continue;
 			if((q-1)%10==0)
+			//使每一行都有10个式子
 				System.out.println();
 			System.out.print(""+q+":"+m+o+n+"=");
 			answer(m,n,ov);
 		}
 	}
 	public static void answer(short m,short n,short ov) {
+		//输出式子结果的函数
 		
 		int x;
 		if(ov==1){
